8b1f6fc8a499a36644da41d4ca860ddcb688e786
JDS <202430328@email.daelim.ac.kr>
2024-09-11 17:18:20 +0900

    처음 커밋

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 100;
+        boolean c = true;
+        char d = 'c';
+        String e = "e;s;d;p";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
---
+++ src/java_22.java
@@ -0,0 +1,7 @@
+public class java_22 {
+    public static void main(String[] args) {
+        char letter = 'A';
+        int asciivalue = letter;
+        System.out.println("The ASCII value of " + letter + " is " + asciivalue);
+    }
+}
